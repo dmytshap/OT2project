d603f8e2f4fb266e84ff2abf2041a5ab2e519689
cabaoba <145194676+Cabaoba@users.noreply.github.com>
2026-02-10 22:15:03 +0200

    Moved email config to environment variables

--- src/mailer/index.php
+++ src/mailer/index.php
@@ -7,53 +7,59 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+use Dotenv\Dotenv;
 
-require 'mailer/Exception.php';
-require 'mailer/PHPMailer.php';
-require 'mailer/SMTP.php';
+require __DIR__ . '/Exception.php';
+require __DIR__ . '/PHPMailer.php';
+require __DIR__ . '/SMTP.php';
 
-//Load Composer's autoloader (created by composer, not included with PHPMailer)
-//require 'vendor/autoload.php';
+//Load Composer's autoloader to enable phpdotenv in dev environments.
+$autoloadPath = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath)) {
+    require $autoloadPath;
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+    $dotenv->safeLoad();
+}
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+// Tarkistetaan onko lomake lähetetty
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['send_email'])) {
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
 
 
-try {
-
-    // -------------------------------------------
-    // This part needs to be in .env file!!!
-    // -------------------------------------------
-    putenv('SMTP_HOST=mail-eu.smtp2go.com');
-    putenv('SMTP_PORT=8465');
-    putenv('SMTP_AUTH=true');
-    putenv('SMTP_USER=...........');
-    putenv('SMTP_PASSWORD=...........');
-    // -------------------------------------------
+    try {
 
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = getenv('SMTP_HOST');                    //Set the SMTP server to send through
-    $mail->Port       = getenv('SMTP_PORT');                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-    $mail->SMTPAuth   = getenv('SMTP_AUTH');                    //Enable SMTP authentication
-    $mail->Username   = getenv('SMTP_USER');                    //SMTP username
-    $mail->Password   = getenv('SMTP_PASSWORD');                //SMTP password
+    $mail->Host       = $_ENV['SMTP_HOST'] ?? '';                    //Set the SMTP server to send through
+    $mail->Port       = $_ENV['SMTP_PORT'] ?? '';                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPAuth   = $_ENV['SMTP_AUTH'] ?? '';                    //Enable SMTP authentication
+    $mail->Username   = $_ENV['SMTP_USER'] ?? '';                    //SMTP username
+    $mail->Password   = $_ENV['SMTP_PASSWORD'] ?? '';                //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
 
     //Recipients
     $mail->setFrom('projektitori@moimail.uk', 'Projektitori');
-    $mail->addAddress('temp1000@moimail.uk', 'Elmo Ilmoittaja');//Add a recipient
+    $mail->addAddress($_ENV['MAIL_TO'] ?? '', $_ENV['MAIL_TO_NAME'] ?? '');//Add a recipient
 //    $mail->addReplyTo('info@example.com', 'Information');
 
     //Content
+    $mail->CharSet = 'UTF-8';                                   //Set charset to UTF-8 for special characters
     $mail->isHTML(true);                                        //Set email format to HTML
     $mail->Subject = 'Kiitos ideasta!';
     $mail->Body    = 'Hei, kiitos kun lähestyit meitä ideallasi. Se oli varmasti <b>LOISTAVA</b>!';
 //    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
+?>
+
+<h1>Sähköpostin testisivu</h1>
+<form method="POST">
+    <button type="submit" name="send_email">Lähetä sähköposti</button>
+</form>
