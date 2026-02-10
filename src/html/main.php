dd6cd3b64fe50ec641f3a241a238720a411a1853
MrGumbert <niko1.oinonen@hotmail.fi>
2026-02-10 20:15:17 +0200

    main.html muutettu main.php, jotta php:ta voi käyttää. database_get_projects_data.php hakee projektit tietokannasta. api-fetcher.php välikappale http pyynnön lähettämiseen.

---
+++ src/html/main.php
@@ -0,0 +1,42 @@
+<?php
+    include '../backend/api-fetcher.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projektitori</title>
+</head>
+
+<body>
+    <link rel="stylesheet" href="../styles/styles_main.css">
+    <header>
+        <p class="projektitori"> <a href="main.html">  Projektitori</p>
+        <nav>
+            <a href="lomake.html"> Lomake</a>
+            <a href="otayhteytta.html"> Ota yhteyttä</a>
+            <a href="login.html"> Kirjaudu sisään</a>
+        </nav>
+    </header>
+
+    <div class="main_page">
+        <div class="main_text_div">
+            <p class="main_text"> Täällä näet kaikki tarjolla olevat projektit. </p>
+        </div>
+        <div class="grid-projects">
+            <?php
+                $url = 'http://localhost/backend/database_get_projects_data.php';
+                //$response = file_get_contents($url);
+                //tai 
+                $response = fetchApiData($url);
+                echo $response;
+            ?>
+        </div>
+    </div>
+
+</body>
+
+</html>
