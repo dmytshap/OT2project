c7e74289f090b31b3fa28c8e5cbd45ae0ebd2f2e
samumoil <samumoil@peikkoposti.com>
2026-02-13 21:16:00 +0200

    updated database schema: user roles, user invites and project reservations

--- docker/init_database.sql
+++ docker/init_database.sql
@@ -17,28 +17,32 @@ USE `PROJECTS` ;
 DROP TABLE IF EXISTS `PROJECTS`.`PROJECT_DATA` ;
 
 CREATE TABLE IF NOT EXISTS `PROJECTS`.`PROJECT_DATA` (
-  `PROJECT_ID` INT NOT NULL AUTO_INCREMENT,
-  `SHORT_DESC` VARCHAR(140) NOT NULL,
-  `PHONE` VARCHAR(45) NOT NULL,
-  `EMAIL` VARCHAR(45) NOT NULL,
-  `COMPANY` VARCHAR(45) NOT NULL,
-  `CONTACT_TIME` DATETIME NOT NULL,
-  `DEADLINE` VARCHAR(45),
-  `LONG_DESC` LONGTEXT NOT NULL,
-  PRIMARY KEY (`PROJECT_ID`)
+  `PROJECT_ID` INT NOT NULL AUTO_INCREMENT
+  , `SHORT_DESC` VARCHAR(140) NOT NULL
+  , `PHONE` VARCHAR(45)
+  , `EMAIL` VARCHAR(45) NOT NULL
+  , `COMPANY` VARCHAR(45)
+  , `CONTACT_TIME` DATETIME NOT NULL
+  , `DEADLINE` VARCHAR(45)
+  , `LONG_DESC` LONGTEXT
+  , `TAG` VARCHAR(45)
+  , `PROJECT_RESERVED` BOOLEAN NOT NULL
+  , PRIMARY KEY (`PROJECT_ID`)
   );
 
 -- -----------------------------------------------------
 -- Insert fake data to PROJECT_DATA
 -- -----------------------------------------------------
 INSERT INTO `PROJECTS`.`PROJECT_DATA`(
-  SHORT_DESC,
-  PHONE,
-  EMAIL,
-  COMPANY,
-  CONTACT_TIME,
-  DEADLINE,
-  LONG_DESC
+  SHORT_DESC
+  , PHONE
+  , EMAIL
+  , COMPANY
+  , CONTACT_TIME
+  , DEADLINE
+  , LONG_DESC
+  , TAG
+  , PROJECT_RESERVED
 ) VALUES (
   "Nettisivu koodausideoiden keräämiseksi"
   , "0401234567"
@@ -47,6 +51,8 @@ INSERT INTO `PROJECTS`.`PROJECT_DATA`(
   , NOW()
   , NULL
   , "Nettisivun tarkoituksena on kerätä ideoita ja helpottaa opettajan työtä. Lorem ipsum dolores magnificum..."
+  , "OT2 2027"
+  , FALSE
 ), (
   "Tämä toinen idea on vielä parempi"
   , "0507654321"
@@ -55,6 +61,8 @@ INSERT INTO `PROJECTS`.`PROJECT_DATA`(
   , NOW()
   , NULL
   , "Mitähän ihmettä tänne kannattaisi kirjoittaa? Tuleeko hyviä ideoita? Tämän tekstin pitäisi olla tarpeeksi pitkä, jotta sen avulla voidaan testata nettisivun ulkoasua. Pystyykö nettisivu näyttämään pitkän tekstin oikein ja huolehtimaan esimerkiksi rivinvaihdosta?"
+  , "gradu"
+  , TRUE
 );
 
 -- -----------------------------------------------------
@@ -63,20 +71,20 @@ INSERT INTO `PROJECTS`.`PROJECT_DATA`(
 DROP TABLE IF EXISTS `PROJECTS`.`USER_SESSION` ;
 
 CREATE TABLE IF NOT EXISTS `PROJECTS`.`USER_SESSION` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `EMAIL` VARCHAR(45) NOT NULL,
-  `OTP_CODE` CHAR(6) NOT NULL,
-  `EXPIRY` DATETIME NOT NULL,
-  PRIMARY KEY (`ID`)
+  `ID` INT NOT NULL AUTO_INCREMENT
+  , `EMAIL` VARCHAR(45) NOT NULL
+  , `OTP_CODE` CHAR(6) NOT NULL
+  , `EXPIRY` DATETIME NOT NULL
+  , PRIMARY KEY (`ID`)
   );
 
 -- -----------------------------------------------------
 -- Insert fake data to USER_SESSION
 -- -----------------------------------------------------
 INSERT INTO `PROJECTS`.`USER_SESSION`(
-  EMAIL,
-  OTP_CODE,
-  EXPIRY
+  EMAIL
+  , OTP_CODE
+  , EXPIRY
 ) VALUES (
   "yeaah@yeaah.com"
   , "MINMAX"
@@ -86,3 +94,71 @@ INSERT INTO `PROJECTS`.`USER_SESSION`(
   , "xAmNIm"
   , ADDTIME(NOW(), '01:01:00') -- Adds 1 hour and 1 minute
 );
+
+-- -----------------------------------------------------
+-- Table `PROJECTS`.`PERMANENT_USERS`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `PROJECTS`.`PERMANENT_USERS` ;
+
+CREATE TABLE IF NOT EXISTS `PROJECTS`.`PERMANENT_USERS` (
+  `EMAIL` VARCHAR(45) NOT NULL
+  , `USER_ROLE` VARCHAR(20) NOT NULL
+  , `CREATED` DATETIME NOT NULL
+  , `EXPIRY` DATETIME
+  , PRIMARY KEY (`EMAIL`)
+  );
+
+-- -----------------------------------------------------
+-- Insert fake data to PERMANENT_USERS
+-- -----------------------------------------------------
+INSERT INTO `PROJECTS`.`PERMANENT_USERS`(
+  EMAIL
+  , USER_ROLE
+  , CREATED
+  , EXPIRY
+) VALUES (
+  "opettaja1@uef.fi"
+  , "admin"
+  , ADDTIME(NOW(), '-111:00:00') -- Puts time 111 hours in the past, time in UTC
+  , ADDTIME(NOW(), '111:00:00') -- Puts time 111 hours in the future, time in UTC
+), (
+  "amanuenssi1@uef.fi"
+  , "admin"
+  , ADDTIME(NOW(), '-100:00:00') -- Puts time 100 hours in the past
+  , ADDTIME(NOW(), '1:00:00') -- Puts time 1 hour in the past
+);
+
+-- -----------------------------------------------------
+-- Table `PROJECTS`.`INVITES`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `PROJECTS`.`INVITES` ;
+
+CREATE TABLE IF NOT EXISTS `PROJECTS`.`INVITES` (
+  `EMAIL` VARCHAR(45) NOT NULL
+  , `LOGIN_CODE` VARCHAR(20) NOT NULL
+  , `CREATED` DATETIME NOT NULL
+  , `EXPIRY` DATETIME NOT NULL
+  , PRIMARY KEY (`EMAIL`)
+  );
+
+-- -----------------------------------------------------
+-- Insert fake data to INVITES
+-- -----------------------------------------------------
+INSERT INTO `PROJECTS`.`INVITES`(
+  EMAIL
+  , LOGIN_CODE
+  , CREATED
+  , EXPIRY
+) VALUES (
+  "opettaja1@uef.fi"
+  , "12345678901234567890"
+  , ADDTIME(NOW(), '-111:00:00') -- Puts time 111 hours in the past, time in UTC
+  , ADDTIME(NOW(), '111:00:00') -- Puts time 111 hours in the future, time in UTC
+), (
+  "amanuenssi1@uef.fi"
+  , "amanuenssinjakkara"
+  , ADDTIME(NOW(), '-100:00:00') -- Puts time 100 hours in the past
+  , ADDTIME(NOW(), '1:00:00') -- Puts time 1 hour in the past
+);
+
+
