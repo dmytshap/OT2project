2ff7ccecc3ea40e9c6dfbfcf7aab873f304fc3fd
MrGumbert <niko1.oinonen@hotmail.fi>
2026-02-25 10:09:45 +0200

    main.php sisältää linkin jokaisen projektin kohdalla, joka näyttää projektin kaikki tiedot project.php. database_get_projects_data.php lisätty projektin haku id:llä. projects_table.php poistettu select elementit.

--- src/backend/database_get_projects_data.php
+++ src/backend/database_get_projects_data.php
@@ -2,8 +2,25 @@
 
 require 'connect_to_database.php';
 
-function getProjectsFromDatabase()
-{
+function getProjectFromDatabase($id){
+    $connection = connectToDatabase();
+
+    $sql = "SELECT * FROM PROJECT_DATA WHERE PROJECT_ID = $id";
+    $result = mysqli_query($connection, $sql);
+
+    $num_of_projects = mysqli_num_rows($result);
+
+    if ($num_of_projects > 0) {
+        $row = $result->fetch_assoc();
+        echo json_encode($row);
+    } else {
+        //TODO better error handling?
+    };
+    $connection->close();
+}
+
+
+function getProjectsFromDatabase(){
 
     $connection = connectToDatabase();
 
@@ -20,6 +37,7 @@ function getProjectsFromDatabase()
         //return json
         echo json_encode($response);
     } else {
+        //TODO better error handling?
         echo 'Ei projekteja';
     }
     $connection->close();
@@ -27,6 +45,10 @@ function getProjectsFromDatabase()
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    getProjectsFromDatabase();
+    if (isset($_GET['id'])) {
+        getProjectFromDatabase($_GET['id']);
+    } else {
+        getProjectsFromDatabase();
+    }
     exit;
 };
--- src/frontend/projects_table.php
+++ src/frontend/projects_table.php
@@ -1,11 +1,11 @@
 <?php
-    include '../backend/export.php';
+include '../backend/export.php';
 ?>
 
 <!DOCTYPE html>
 <!--This version is with export to csv possibility-->
 <html>
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -16,36 +16,35 @@
 </head>
 
 <body>
+    <!--sends POST request to backend-->
     <form action="../backend/export.php" method="post">
         <input type="submit" value="Export to csv">
     </form>
 
-<table>
-    <th>Valitse</th>
-    <th>Projekti ID</th>
-    <th>Projektin nimi</th>
-    <th>Lyhytkuvaus</th>
-    <th>Puhelin</th>
-    <th>Sähköposti</th>
-    <th>Yritys</th>
-    <th>Ota yhteyttä</th>
-    <th>Deadline</th>
-    <th>Pitkäkuvaus</th>
-    <th>Tag</th>
-    <th>Projekti varattu</th>
+    <table>
+        <th>Projekti ID</th>
+        <th>Projektin nimi</th>
+        <th>Lyhytkuvaus</th>
+        <th>Puhelin</th>
+        <th>Sähköposti</th>
+        <th>Yritys</th>
+        <th>Ota yhteyttä</th>
+        <th>Deadline</th>
+        <th>Pitkäkuvaus</th>
+        <th>Tag</th>
+        <th>Projekti varattu</th>
 
-    <?php
+        <?php
         $url = 'http://localhost/backend/database_get_projects_data.php';
         //returns json
         $response = file_get_contents($url);
         //decodes json into array
         $data = json_decode($response, true);
-        
-        foreach($data as $row) {
+
+        foreach ($data as $row) {
             echo "<tr>";
             //select if you want to select specific rows to export
             //TO DO how to do this?
-            echo "<td><input type='checkbox' name='select'></td></a>";
             echo "<td> $row[PROJECT_ID]</td>";
             echo "<td> $row[PROJECT_NAME]</td>";
             echo "<td> $row[SHORT_DESC]</td>";
@@ -59,8 +58,9 @@
             echo "<td> $row[TAG]</td>";
             echo "<td> $row[PROJECT_RESERVED]</td>";
             echo "</tr>";
-        }      
-    ?>
-</table>
+        }
+        ?>
+    </table>
 </body>
+
 </html>
