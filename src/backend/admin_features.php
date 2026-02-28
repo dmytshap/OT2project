96e8697d00b8978c2b7835725bd736a421d416c1
Dmytro Shapovalov <dimashapovalov0011@gmail.com>
2026-03-01 00:22:30 +0200

    Link deletion + copying to clipboard Tokens are now shown in DESC order

--- src/backend/admin_features.php
+++ src/backend/admin_features.php
@@ -5,35 +5,62 @@ if (session_status() === PHP_SESSION_NONE) {
 
 require 'connect_to_database.php';
 
-$email = $_POST['teacher_email'];
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['add_teacher'])) {
-        addTeacherGiveRole($email);
+
+    if (isset($_POST['add_teacher']) || isset($_POST['delete_teacher'])) {
+        $email = $_POST['teacher_email'];
+
+        if (isset($_POST['add_teacher'])) {
+            addTeacherGiveRole($email);
+        }
+
+        if (isset($_POST['delete_teacher'])) {
+            deleteTeacher($email);
+        }
+
+        header("Location: /frontend/admin_panel.php");
+        exit();
     }
-    if (isset($_POST['delete_teacher'])) {
-        deleteTeacher($email);
+    if (isset($_POST['delete_invite_links'])) {
+        $tokens = $_POST['selected_tokens'] ?? [];
+
+        if (!empty($tokens)) {
+            $deleted = deleteInviteLink($tokens);
+            $_SESSION['message'] = "$deleted kutsulinki(t) poistettu";
+            $_SESSION['message_type'] = "success";
+        } else {
+            $_SESSION['message'] = "Valitse linkit poistaaksesi.";
+            $_SESSION['message_type'] = "warning";
+        }
+    }
+    if (isset($_POST['luo_kutsulinkki'])) {
+        include 'generate_invite_link.php';
+        $invite_link = generateInviteLink();
+        // tätä tarvitaan että tulisi ilmoitus alertUser() admin_panel.php:ssä.
+        $_SESSION['invite_link'] = $invite_link;
+        header("Location: /frontend/admin_panel.php");
+        exit();
     }
 
     header("Location: /frontend/admin_panel.php");
-    exit();
-}
+} // if post
 
 
 // Adds teacher to database and gives role
-function addTeacherGiveRole($email) {
-    
+function addTeacherGiveRole($email)
+{
+
     $connection = connectToDatabase();
     $email = $_POST['teacher_email'];
     $role = "TEACHER";
     $created = date("Y-m-d H:i:s");
     $expiry = date("Y-m-d H:i:s", strtotime("+99 year"));
 
-    $stmtCheckTeacherInDB = $connection -> prepare("SELECT 1 FROM PERMANENT_USERS WHERE EMAIL = ? LIMIT 1");
+    $stmtCheckTeacherInDB = $connection->prepare("SELECT 1 FROM PERMANENT_USERS WHERE EMAIL = ? LIMIT 1");
     $stmtCheckTeacherInDB->bind_param("s", $email);
     $stmtCheckTeacherInDB->execute();
     $stmtCheckTeacherInDB->store_result();
-    
+
     //Notification
     if ($stmtCheckTeacherInDB->num_rows > 0) {
         $_SESSION['message'] = "Email is already a teacher.";
@@ -46,8 +73,8 @@ function addTeacherGiveRole($email) {
     if (!str_ends_with(strtolower($email), 'uef.fi')) {
         die("Vain @uef.fi sähköpostit ovat sallitut opettajille.");
     }
-    $stmt = $connection->prepare("INSERT INTO PERMANENT_USERS (EMAIL,USER_ROLE,CREATED,EXPIRY) VALUES(?,?,?,?)");    
-    $stmt -> bind_param("ssss",$email,$role,$created, $expiry);
+    $stmt = $connection->prepare("INSERT INTO PERMANENT_USERS (EMAIL,USER_ROLE,CREATED,EXPIRY) VALUES(?,?,?,?)");
+    $stmt->bind_param("ssss", $email, $role, $created, $expiry);
 
     //Notification
     if ($stmt->execute()) {
@@ -61,15 +88,16 @@ function addTeacherGiveRole($email) {
     $connection->close();
 }
 // Deletes teacher from PERMANENT_USERS table
-function deleteTeacher($email) {
+function deleteTeacher($email)
+{
     $connection = connectToDatabase();
     $email = $_POST['teacher_email'];
 
-    $stmt = $connection-> prepare("DELETE FROM PERMANENT_USERS WHERE EMAIL = ?");
+    $stmt = $connection->prepare("DELETE FROM PERMANENT_USERS WHERE EMAIL = ?");
     $stmt->bind_param("s", $email);
     $stmt->execute();
 
-     if ($stmt->affected_rows > 0) {
+    if ($stmt->affected_rows > 0) {
         $_SESSION['message'] = "Teacher deleted successfully.";
         $_SESSION['message_type'] = "success";
     } else {
@@ -82,4 +110,25 @@ function deleteTeacher($email) {
 }
 
 
-?>
+function deleteInviteLink(array $chosen_tokens)
+{
+    if (empty($chosen_tokens)) {
+        return false;
+    }
+    #Prepared Statement varten, laskee montako tokenejä on valittu ja laittaa niittää $stmt:iin.
+    $placeholders = implode(',', array_fill(0, count($chosen_tokens), '?'));
+
+    $connection = connectToDatabase();
+    $stmt = $connection->prepare("DELETE FROM INVITE_LINKS WHERE TOKEN IN ($placeholders)");
+
+    #s:ien määrä #stmt varten.
+    $strings_amount = str_repeat("s", count($chosen_tokens));
+    $stmt->bind_param($strings_amount, ...$chosen_tokens);
+
+    $stmt->execute();
+    $deleted = $stmt->affected_rows;
+    $stmt->close();
+    $connection->close();
+
+    return $deleted;
+}
--- src/frontend/admin_panel.php
+++ src/frontend/admin_panel.php
@@ -5,12 +5,24 @@ if (session_status() === PHP_SESSION_NONE) {
 
 require_once '../backend/connect_to_database.php';
 $connection = connectToDatabase();
-$sql = 'SELECT * FROM INVITE_LINKS';
+$sql = 'SELECT * FROM INVITE_LINKS ORDER BY TOKEN_EXPIRY DESC';
 $result = mysqli_query($connection, $sql);
 
+if (isset($_POST['delete_invite_links']) && isset($_POST['selected_tokens'])) {
+    $deleted = deleteInviteLink($_POST['selected_tokens']);
+
+    $_SESSION['messages'] = "$deleted kutsulinki(t) on poistettu / ovat poistettu";
+    $_SESSION['message_type'] = "success";
+
+    header("Location: ../frontend/admin_panel.php");
+    exit();
+}
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,105 +31,130 @@ $result = mysqli_query($connection, $sql);
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles/styles_admin_panel.css">
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-3 fw-semibold sticky-top">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="uusi_main.php">Projektitori</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fw-normal">
-                        <li class="nav-item">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="uusi_main.php">Projektitori</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fw-normal">
+                    <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="uusi_lomake.php">Lomake</a>
-                        </li>
-                        <li class="nav-item">
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="hallinta.php">Projektien hallinta</a>
-                        </li>
-                        <li class="nav-item dropdown">
+                    </li>
+                    <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Opettajan nimi
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li><a class="dropdown-item" href="logout.php">Kirjaudu ulos</a></li>
-                            
-                    </ul>                                   
-                        </li>
-                    </ul>
-                    </div>
-                </div>
-    </nav>
-    <div class="container mt-4">
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a class="breadcrumb-ptori" href="uusi_main.php">Projektitori</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Admin</li>
-        </ol>
-    </nav>
-    <h2 class="main_text mb-4">Admin</h2>
-    <!---Notification--->
-    <?php if (isset($_SESSION['message'])): ?>
-    <div class="alert alert-<?php echo $_SESSION['message_type']; ?>">
-        <?php 
-            echo $_SESSION['message']; 
-            unset($_SESSION['message']);
-            unset($_SESSION['message_type']);
-        ?>
-    </div>
-    <?php endif; ?>
-    <h4 class= "valiotsikko">Opettajien hallinta</h4>
-    <form action = "/backend/admin_features.php" method="post" class="row g-4">
-        <div class="col-md-6">
-            <label for="inputProjektiNimi" class="form-label">Opettajan sähköposti (vain @uef.fi)</label>
-            <input type="text" class="form-control" name="teacher_email" id="teacherEmail" placeholder="Syötä opettajan sähköposti">
-            <button class="btn-lisaa-opettaja px-5 py-2"  id="addTeacher" name="add_teacher"> Lisää opettaja </button>
-            <button class="btn-poista-opettaja px-5 py-2"  id="deleteTeacher" name="delete_teacher" > Poista opettaja </button>
 
-        </div>
-    </form>
-        <h4 class= "valiotsikko">Kutsulinkit</h4>
-        <div class="button-div d-flex align-items-center justify-content-between w-100 mb-2">
-            <h6 class="aktiiviset-kutsulinkit mb-0">Aktiiviset kutsulinkit</h6>
-            <button class="btn-kutsulinkki px-5 py-2" id="luoKutsulinkki" name="luoKutsulinkki">Luo uusi kutsulinkki</button>
-        </div>
-        <div class="table-responsive">
-            <table class="table table-hover shadow">
-                <thead>
-                    <tr>
-                    <th scope="col"><input class='form-check-input' type="checkbox" id="select-all" /></th>    
-                    <th scope="col">Token</th>
-                    <th scope="col">Vanhenemisaika</th>
-                    </tr>
-                </thead>
-                <tbody class="table-group-divider">
-                    <?php
-                    while($row = mysqli_fetch_assoc($result)){
-
-                        echo "<tr>
-                            <th scope='col'><input class='form-check-input row-checkbox' type='checkbox' /></th>
-                            <td>$row[TOKEN]</td>
-                            <td>$row[TOKEN_EXPIRY]</td>
-                        </tr> ";
-
-                    }
-                    
-                    ?>
-                </tbody>
-            </table>
-        </div>
-        <div id="toolbar" class="toolbar d-none d-flex shadow justify-content-between align-items-center p-3 bg-light border rounded mt-2">
-            <span id="selected-count">1 valittu</span>
-            <div class="scroll">
-                <button id="btn-poista" class="btn btn-poista">Poista käytöstä</button>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
+    </nav>
+    <div class="container mt-4">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a class="breadcrumb-ptori" href="uusi_main.php">Projektitori</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Admin</li>
+            </ol>
+        </nav>
+        <h2 class="main_text mb-4">Admin</h2>
+        <!---Notification--->
+        <?php if (isset($_SESSION['message'])): ?>
+            <div class="alert alert-<?php echo $_SESSION['message_type']; ?>">
+                <?php
+                echo $_SESSION['message'];
+                unset($_SESSION['message']);
+                unset($_SESSION['message_type']);
+                ?>
+            </div>
+        <?php endif; ?>
+        <h4 class="valiotsikko">Opettajien hallinta</h4>
+        <form action="/backend/admin_features.php" method="post" class="row g-4">
+            <div class="col-md-6">
+                <label for="teacherEmail" class="form-label">Opettajan sähköposti (vain @uef.fi)</label>
+                <input type="text" class="form-control" name="teacher_email" id="teacherEmail" placeholder="Syötä opettajan sähköposti">
+                <button class="btn-lisaa-opettaja px-5 py-2" id="addTeacher" name="add_teacher"> Lisää opettaja </button>
+                <button class="btn-poista-opettaja px-5 py-2" id="deleteTeacher" name="delete_teacher"> Poista opettaja </button>
+
+            </div>
+        </form>
+
+        <form action="/backend/admin_features.php" method="post"> <!-- vois lisätä class="row g-4" tänne jos halua -->
+            <h4 class="valiotsikko">Kutsulinkit</h4>
+
+            <div class="button-div d-flex align-items-center justify-content-between w-100 mb-2">
+                <h6 class="aktiiviset-kutsulinkit mb-0">Aktiiviset kutsulinkit</h6>
+                <button class="btn-kutsulinkki px-5 py-2" id="luoKutsulinkki" name="luo_kutsulinkki" type="submit" onClick="alertUser()" value="Show alert Box">Luo uusi kutsulinkki</button>
+            </div>
+        </form>
+        <form action="/backend/admin_features.php" method="post">
+            <div class="table-responsive">
+                <table class="table table-hover shadow">
+                    <thead>
+                        <tr>
+                            <th scope="col"><input class='form-check-input' type="checkbox" id="select-all" /></th>
+                            <th scope="col">Token</th>
+                            <th scope="col">Vanhenemisaika</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <?php
+                        while ($row = mysqli_fetch_assoc($result)): ?>
+                            <tr>
+                                <td>
+                                    <input class='form-check-input row-checkbox'
+                                        type='checkbox'
+                                        name='selected_tokens[]'
+                                        value='<?= htmlspecialchars($row['TOKEN']) ?>' />
+                                </td>
+                                <td><?= htmlspecialchars($row['TOKEN']) ?></td>
+                                <td><?= htmlspecialchars($row['TOKEN_EXPIRY']) ?></td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+            </div>
+            <div id="toolbar" class="toolbar d-none d-flex shadow justify-content-between align-items-center p-3 bg-light border rounded mt-2">
+                <span id="selected-count">1 valittu</span>
+                <div class="scroll">
+                    <button id="btn-kopioi" type="button" name="copy_invite_links" class="btn btn-kopioi"> Kopioi linkki</button>
+                    <button id="btn-poista" type="submit" name="delete_invite_links" class="btn btn-poista">Poista käytöstä</button>
+                </div>
+            </div>
+        </form>
     </div>
-      
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>        
-       
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <!-- LLM:n tuottama, katsoin tutorialit netissä niin siellä tehtiin samalla tavalla -->
+    <script>
+        document.getElementById('btn-kopioi').addEventListener('click', function(e) {
+            e.preventDefault(); 
+            const checked = document.querySelectorAll('.row-checkbox:checked');
+
+            const links = Array.from(checked).map(cb => {
+                return window.location.origin + '/frontend/uusi_lomake.php?token=' + cb.value;   // Tätä pitää sitten muokkaa, kun tehdään mod_rewrite jutun, että olisi custom linkkejä tiedostoille. Eli esim backend/admin_panel.php sijaan olisi ihan vaa localhost/adminPanel
+            }).join('\n');
+
+            navigator.clipboard.writeText(links).then(() => {
+                alert(checked.length + ' linkki(ä) kopioitu leikepöydälle!');
+            }).catch(() => {
+                alert('Kopiointi epäonnistui.');
+            });
+        });
+    </script>
     <!-- Tekoälyn luoma koodi, toolbar tulee näkyville, kun valitaan 1 tai useampi checkbox -->
     <script>
-        
         const selectAll = document.getElementById('select-all');
         const rowCheckboxes = document.querySelectorAll('.row-checkbox');
         const toolbar = document.getElementById('toolbar');
@@ -133,7 +170,7 @@ $result = mysqli_query($connection, $sql);
                 toolbar.classList.remove('d-none');
                 selectedCount.textContent = `${count} valittu`;
             } else {
-                toolbar.classList.add('d-none');  // piilotetaan toolbar
+                toolbar.classList.add('d-none'); // piilotetaan toolbar
                 selectedCount.textContent = '0 valittu';
             }
 
@@ -148,5 +185,15 @@ $result = mysqli_query($connection, $sql);
             updateToolbar();
         });
     </script>
+
+    <?php if (isset($_SESSION['invite_link'])): ?>
+        <script>
+            window.onload = function() {
+                alert("Tässä on kutsulinkki:\n<?= $_SESSION['invite_link'] ?>");
+            }
+        </script>
+        <?php unset($_SESSION['invite_link']); ?>
+    <?php endif; ?>
 </body>
+
 </html>
